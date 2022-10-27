454a44c1a7676b85f20bed362b55cf4cd4784bff
sarahmoore <sarahnmoore19@gmail.com>
2022-10-27 04:09:01 -0400

    created DynamicArray class with shift/unshift/pop/push methods w/o built-ins

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,47 +1,66 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(this.capacity)
 
-    // Your code here
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
   }
 
   push(val) {
-
-    // Your code here
+    if (this.length === this.capacity) this.resize();
+    this.length++;
+    this.data[this.length - 1] = val;
   }
 
-
   pop() {
-
-    // Your code here
+    let pop = this.data[this.length - 1];
+    this.length--;
+    let value = (this.capacity - this.length)
+    this.data.length-= value;
+    this.data.length+= value;
+    return pop;
   }
 
   shift() {
-
-    // Your code here
+    let shift = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    this.length--;
+    let value = (this.capacity - this.length)
+    this.data.length-= value;
+    this.data.length+= value;
+    return shift;
   }
 
   unshift(val) {
-
-    // Your code here
+    if (this.length === this.capacity) this.resize();
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.length++;
+    this.data[0] = val;
   }
 
   indexOf (val) {
-
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) return i;
+    }
+    return -1;
   }
 
   resize () {
-
-    // Your code here
+    this.capacity *= 2;
+    this.data.length = this.capacity;
   }
 
 }
+
 
 
 module.exports = DynamicArray;
